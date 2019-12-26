b42479ab94e154615f597bded52b8471ef827124
HENDRIX-ZT2 <arnfinn@spholderer.de>
2019-12-26 23:14:47 +0100

    Update matcol import Fixed transforms with nodegroups, need to be verified Blending and blend order is still off

--- import_matcol.py
+++ import_matcol.py
@@ -3,6 +3,7 @@ import io
 import sys
 import bpy
 import mathutils
+import math
 
 from .pyffi_ext.formats.materialcollection import MaterialcollectionFormat
 from .pyffi_ext.formats.fgm import FgmFormat
@@ -32,6 +33,154 @@ def load_tex(tree, tex_path):
 
 	return tex
 
+def create_flip():
+	name = "FlipX"
+	#only create the material if we haven't already created it, then just grab it
+	if name not in bpy.data.node_groups:
+		# create a group
+		test_group = bpy.data.node_groups.new(name, 'ShaderNodeTree')
+
+	else:
+		test_group = bpy.data.node_groups[name]
+		for node in test_group.nodes:
+			test_group.nodes.remove(node)
+		for node in test_group.inputs:
+			test_group.inputs.remove(node)
+		for node in test_group.outputs:
+			test_group.outputs.remove(node)
+
+	# create group inputs
+	group_inputs = test_group.nodes.new('NodeGroupInput')
+	group_inputs.location = (-350,0)
+	test_group.inputs.new('NodeSocketVectorXYZ','in')
+
+	# create group outputs
+	group_outputs = test_group.nodes.new('NodeGroupOutput')
+	group_outputs.location = (300,0)
+	test_group.outputs.new('NodeSocketVectorXYZ','out')
+
+
+	split = test_group.nodes.new('ShaderNodeSeparateXYZ')
+	split.label = "Split"
+	test_group.links.new(group_inputs.outputs["in"], split.inputs[0])
+	
+	flip = test_group.nodes.new('ShaderNodeMath')
+	flip.operation = 'MULTIPLY'
+	test_group.links.new(split.outputs[1], flip.inputs[0])
+	flip.inputs[1].default_value = -1.0
+	
+	join = test_group.nodes.new('ShaderNodeCombineXYZ')
+	join.label = "Join"
+	test_group.links.new(flip.outputs[0], join.inputs[0])
+	test_group.links.new(split.outputs[1], join.inputs[1])
+	test_group.links.new(split.outputs[2], join.inputs[2])
+	
+	# #link output
+	test_group.links.new(join.outputs[0], group_outputs.inputs['out'])
+	
+	nodes_iterate(test_group, group_outputs)
+	return test_group
+
+def create_group():
+	flipgr = create_flip()
+	name = "MatcolSlot"
+	#only create the material if we haven't already created it, then just grab it
+	if name not in bpy.data.node_groups:
+		# create a group
+		test_group = bpy.data.node_groups.new(name, 'ShaderNodeTree')
+
+	else:
+		test_group = bpy.data.node_groups[name]
+		for node in test_group.nodes:
+			test_group.nodes.remove(node)
+		for node in test_group.inputs:
+			test_group.inputs.remove(node)
+		for node in test_group.outputs:
+			test_group.outputs.remove(node)
+
+	# create group inputs
+	group_inputs = test_group.nodes.new('NodeGroupInput')
+	test_group.inputs.new('NodeSocketVectorTranslation','UVOffset')
+	test_group.inputs.new('NodeSocketFloatAngle','uvRotationAngle')
+	test_group.inputs.new('NodeSocketVectorTranslation','uvRotationPosition')
+	test_group.inputs.new('NodeSocketVectorXYZ','uvTile')
+
+	# create group outputs
+	group_outputs = test_group.nodes.new('NodeGroupOutput')
+	group_outputs.location = (300,0)
+	test_group.outputs.new('NodeSocketVectorXYZ','out')
+
+
+	offset_flipx = test_group.nodes.new("ShaderNodeGroup")
+	offset_flipx.node_tree = flipgr
+	test_group.links.new(group_inputs.outputs["UVOffset"], offset_flipx.inputs[0])
+	
+	rotpos_flipx = test_group.nodes.new("ShaderNodeGroup")
+	rotpos_flipx.node_tree = flipgr
+	test_group.links.new(group_inputs.outputs["uvRotationPosition"], rotpos_flipx.inputs[0])
+
+	uv = test_group.nodes.new('ShaderNodeUVMap')
+	uv.label = "UV Input"
+	uv.uv_map = "UV0"
+	
+	scale_pivot = test_group.nodes.new('ShaderNodeMapping')
+	scale_pivot.inputs[1].default_value[1] = -1.0
+	scale_pivot.label = "Scale Pivot"
+	test_group.links.new(uv.outputs[0], scale_pivot.inputs[0])
+
+
+
+	uv_offset = test_group.nodes.new('ShaderNodeMapping')
+	uv_offset.label = "UVOffset"
+	test_group.links.new(scale_pivot.outputs[0], uv_offset.inputs[0])
+	test_group.links.new(offset_flipx.outputs[0], uv_offset.inputs[1])
+	
+	uv_tile = test_group.nodes.new('ShaderNodeMapping')
+	uv_tile.label = "uvTile"
+	test_group.links.new(uv_offset.outputs[0], uv_tile.inputs[0])
+	test_group.links.new(group_inputs.outputs["uvTile"], uv_tile.inputs[3])
+	
+	rot_pivot = test_group.nodes.new('ShaderNodeMapping')
+	rot_pivot.inputs[1].default_value[1] = -1.0
+	rot_pivot.label = "Rot Pivot"
+	test_group.links.new(uv_tile.outputs[0], rot_pivot.inputs[0])
+	
+	uv_rot_pos_a = test_group.nodes.new('ShaderNodeMapping')
+	uv_rot_pos_a.label = "uvRotationPosition"
+	test_group.links.new(rot_pivot.outputs[0], uv_rot_pos_a.inputs[0])
+	test_group.links.new(rotpos_flipx.outputs[0], uv_rot_pos_a.inputs[1])
+	
+	# extra step to create vector from float
+	uv_rot_combine = test_group.nodes.new('ShaderNodeCombineXYZ')
+	uv_rot_combine.label = "build uvRotation Vector"
+	test_group.links.new(group_inputs.outputs["uvRotationAngle"], uv_rot_combine.inputs[2])
+	
+	
+	uv_rot = test_group.nodes.new('ShaderNodeMapping')
+	uv_rot.label = "uvRotationAngle"
+	test_group.links.new(uv_rot_pos_a.outputs[0], uv_rot.inputs[0])
+	test_group.links.new(uv_rot_combine.outputs[0], uv_rot.inputs[2])
+
+	# extra step to negate input
+	uv_rot_pos_flip = test_group.nodes.new('ShaderNodeVectorMath')
+	uv_rot_pos_flip.operation = "SCALE"
+	uv_rot_pos_flip.label = "flip uvRotationPosition"
+	# counter intuitive index for non-vector argument!
+	uv_rot_pos_flip.inputs[2].default_value = -1.0
+	test_group.links.new(rotpos_flipx.outputs[0], uv_rot_pos_flip.inputs[0])
+	
+	uv_rot_pos_b = test_group.nodes.new('ShaderNodeMapping')
+	uv_rot_pos_b.label = "undo uvRotationPosition"
+	test_group.links.new(uv_rot_pos_flip.outputs[0], uv_rot_pos_b.inputs[1])
+	test_group.links.new(uv_rot.outputs[0], uv_rot_pos_b.inputs[0])
+	
+	# #link output
+	test_group.links.new(uv_rot_pos_b.outputs[0], group_outputs.inputs['out'])
+	
+	
+	nodes_iterate(test_group, group_outputs)
+	return test_group
+
 def create_material(matcol_path):
 	slots = load_matcol(matcol_path)
 
@@ -44,8 +193,11 @@ def create_material(matcol_path):
 	#only create the material if we haven't already created it, then just grab it
 	else:
 		mat = bpy.data.materials[matname]
+
+	
 	mat.use_nodes = True
 	
+	group = create_group()
 	tree = mat.node_tree
 	# clear default nodes
 	for node in tree.nodes:
@@ -79,23 +231,28 @@ def create_material(matcol_path):
 		mask_path = os.path.join(matdir, matname+".playered_blendweights_{:02}.png".format(i))
 		mask = load_tex(tree, mask_path)
 
-		uv = tree.nodes.new('ShaderNodeUVMap')
-		uv.name = "TexCoordIndex"+str(i)
-		uv.uv_map = "UV0"
-		
-		transform = tree.nodes.new('ShaderNodeMapping')
-		#todo: negate V coordinate
-		matrix_4x4 = mathutils.Matrix()
-		uvscale = list(i for i in infos[7].info.value)[:3]
-		transform.inputs[3].default_value = uvscale
 
-		# in radians for both blender & game matcol
-		rot = infos[5].info.value[0]
+		transform = tree.nodes.new("ShaderNodeGroup")
+		transform.node_tree = group
+
+		# m_uvRotationPosition
+		uvrotpos = list(i for i in infos[6].info.value)[:3]
+		transform.inputs["uvRotationPosition"].default_value = uvrotpos
+
+		# m_UVOffset
+		uvoffset = list(i for i in infos[4].info.value)[:3]
+		transform.inputs["UVOffset"].default_value = uvoffset
+
+		# m_uvTile
+		uvscale = list(i for i in infos[7].info.value)[:3]
+		transform.inputs["uvTile"].default_value = uvscale
+
+		# m_uvRotationAngle
+		# matcol stores it as fraction of 180°
+		# in radians for blender internally even though it displays as degree
+		rot = math.radians( infos[5].info.value[0]*180 )
 		# flip since blender flips V coord
-		transform.inputs[2].default_value[2] = -rot
-		# transform.translation = matrix_4x4.to_translation()
-		transform.name = "TextureTransform"+str(i)
-		tree.links.new(uv.outputs[0], transform.inputs[0])
+		transform.inputs["uvRotationAngle"].default_value = -rot
 		tree.links.new(transform.outputs[0], tex.inputs[0])
 		
 		tex.update()
